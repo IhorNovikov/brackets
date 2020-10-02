b55d693af49a888eb6d30fd58ee5e4444e57cd4d
Ihor Novikov <novikov8118@gmail.com>
2020-10-02 20:28:55 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+module.exports = check = (str, bracketsConfig) => {
+    if (str.match(/\d/)) {
+        return Number.isInteger(([...str].reduce((s, c) => s + +c, 0)) / [...bracketsConfig].flat().reduce((s, c) => s + +c, 0))
+    } else {
+        if (str.match(/\[\(\]|\(\)\)\(|\|\(\|\)|\[\]\]\[\[\]/)) {
+            return false
+        }
+        return ((str.match(/\)/g) ? str.match(/\)/g).length : 0) + (str.match(/\(/g) ? str.match(/\(/g).length : 0) +
+            (str.match(/\]/g) ? str.match(/\]/g).length : 0) + (str.match(/\[/g) ? str.match(/\[/g).length : 0) +
+            (str.match(/\}/g) ? str.match(/\}/g).length : 0) + (str.match(/\{/g) ? str.match(/\{/g).length : 0) +
+            (str.match(/\|/g) ? str.match(/\|/g).length : 0)) % 2 == 0 ? true : false
+    }
 }
